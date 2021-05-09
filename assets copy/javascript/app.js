62f2deb3f81b9d597e9ce9dffbcb0612639e0c16
Jakeokony1024 <jakeokony@gmail.com>
2021-05-08 22:08:14 -0400

    project buttons populate on portfolio modal

--- assets copy/javascript/app.js
+++ assets copy/javascript/app.js
@@ -2,7 +2,6 @@
 $(document).ready(function(){
 
     const projectArray = [
-
         {
             image: "./assets copy/images/workoutplaceholder1.jpeg",
             title: "Corey's Active Fitness",
@@ -11,123 +10,139 @@ $(document).ready(function(){
                 website: "https://coreysactivefitness.herokuapp.com/",
                 github: "https://github.com/jakeokony1024/Coreys-Active-Fitness",
             },
-            // software: {
-            //     javaScript: "",
-            //     JQuery: "",
-            //     CSS : "",
-            //     HTML: "", 
-            //     Express: "", 
-            //     Handlebars: "",
-            //     NodeJS: "",
-            //     MySQL: "", 
-            //     Sequelize: "",
-            // },
-            // },
+            software: {
+                javaScript: "",
+                JQuery: "",
+                CSS : "",
+                HTML: "", 
+                Express: "", 
+                Handlebars: "",
+                NodeJS: "",
+                MySQL: "", 
+                Sequelize: "",
+            },
+        },
     
-            // {
-            //     image: "",
-            //     title: "",
-            //     description: "",
-            //     nks : {
-            //         website: "",
-            //         github: "",
-            //     },
-            //     software: [],
-            // },
+            {
+                image: "./assets copy/images/workoutplaceholder1.jpeg",
+                title: "Mikes Movie Search",
+                description: "This site is designed to help the client search for a movie or tv show and directs them to the streaming service that is offering that show or movie.",
+                links : {
+                    website: "https://jakeokony1024.github.io/Mikes-Movie-Search/",
+                    github: "https://github.com/jakeokony1024/Mikes-Movie-Search",
+                },
+                software: {
+                    javaScript: "",
+                    JQuery: "",
+                    CSS : "",
+                    HTML: "", 
+                    AJAX: ""
+                },
+            },
     
-            // {
-            //     image: "",
-            //     title: "",
-            //     description: "",
-            //     nks : {
-            //         website: "",
-            //         github: "",
-            //     },
-            //     software: [],
+            {
+                image: "./assets copy/images/workoutplaceholder1.jpeg",
+                title: "Image Click Game",
+                description: "This site gives the user a table of images and directions to try and click on each different image no more than once. Once an image is clicked, the entire table is rearranged in a random order, and if the same image is clicked twice, the score is set back to zero. ",
+                links : {
+                    website: "https://jakeokony1024.github.io/clickgame-2/",
+                    github: "https://github.com/jakeokony1024/clickgame-2",
+                },
+                software: {
+                    javaScript: "",
+                    JQuery: "",
+                    CSS : "",
+                    HTML: "", 
+                    React: ""
+                },
     
-            // },
+            },
     
-            // {
-            //     image: "",
-            //     title: "",
-            //     description: "",
-            //     nks : {
-            //         website: "",
-            //         github: "",
-            //     },
-            //     software: [],
+            {
+                image: "./assets copy/images/workoutplaceholder1.jpeg",
+                title: "Friend Finder App",
+                description: "This site has the user go through a list of questions, and based on the answers gives them a 'friend' they are most compatible with.",
+                links : {
+                    website: "https://dry-temple-36787.herokuapp.com/",
+                    github: "https://github.com/jakeokony1024/FriendFinder",
+                },
+                software: {
+                    JavaScript: "",
+                    Express: "",
+                    Path: "",
+                    CSS: "", 
+                    HTML: "",
+                    NodeJs: ""
+                },
     
-            // },
+            },
     
-            // {
-            //     image: "",
-            //     title: "",
-            //     description: "",
-            //     nks : {
-            //         website: "",
-            //         github: "",
-            //     },
-            //     software: [],
+            {
+                image: "./assets copy/images/workoutplaceholder1.jpeg",
+                title: "Giphy Image Search",
+                description: "This site allows users to search for a list of GIF images populated from the Giphy API. Images can be clicked to show the animation, and clicked again to stop animation.",
+                links : {
+                    website: "https://jakeokony1024.github.io/Giphy-App/",
+                    github: "https://github.com/jakeokony1024/Giphy-App",
+                },
+                software: {
+                    javaScript: "",
+                    JQuery: "",
+                    CSS : "",
+                    HTML: "", 
+                    AJAX: ""
+                },
     
         },
-    ];
-    
-    
-    $("#clearbtn").on("cck", function () {
-        $("#name").empty();
-        $("#email").empty();
-        $("#message").empty();
-    })
-    
-    
-    ///////// ABOUT ME MODAL///////////
-    let aboutModal = document.getElementById("aboutModal");
-    let aboutButton = document.getElementById("aboutButton");
-    let aboutClose = document.getElementsByClassName("aboutClose")[0];
-    let portClose = document.getElementsByClassName("portClose")[0];
-    ////////PORTFOO MODAL/////////////
-    let portfolioModal = document.getElementById("portfolioModal");
-    let portfolioButton = document.getElementById("portfolioButton");
-    
-
+];
+    let projectTitle;
+    let projectDescription;
+    let projectImage ;
+    let projectLinks = [];
+    ////Modal Button Function////
     jQuery(':button').click(function () {
         if (this.id == 'aboutButton') {
+            console.log(aboutModal);
             aboutModal.style.display = "block";
             portfolioModal.style.display = "none";
         }
         else if (this.id == 'portfolioButton') {
             portfolioModal.style.display = "block";
             aboutModal.style.display = "none";
+            let projectDiv = $("#projectButtons");
+            if ($('#projectButtons').is(':empty')){
+                projectButtons();
+            }else{
+                return;
+            }
         }
-        
-        // else if (window.click()) {
-        //     aboutModal.style.display = "none";
-        //     portfolioModal.style.display = "none";
-        // }
-    });
-    // aboutButton.onclick = () => aboutModal.style.display = "block";
+        else if (this.id == 'aboutClose') {
+            aboutModal.style.display = "none";
+        }
+        else if (this.id == 'portClose') {
+            portfolioModal.style.display = "none";
+        }
+        else if(this.id == 'projectButton') {
+            projectModal.style.display = "block";
+        }
+        else if(this.id =='projectClose') {
+            projectModal.style.display = "none";
+        }
+    }); 
+
+    let projectButtons = () => {
+        //iterate through projectArray object, grab out titles, descriptions, images, links, software list//
+        projectArray.forEach((element, index, array) => {
+            a = $("<button class='btn btn-primary'>");
+            console.log(element.title);
+            a.text(element.title);
+            $("#projectButtons").append(a);
+        })
+    }
     
-    
-    // portfolioButton.onclick = () => {
-    //     portfolioModal.style.display = "block";
-    //     // $("port-title").
-    // }
-    
-    // aboutClose.onclick = () => {
-    //     aboutModal.style.display = "none";
-    // };
-    
-    // portClose.onclick = () => {
-    //     portfolioModal.style.display = "none";
-    // };
-    
-    // window.oncck = e => {
-    //     e.preventDefault();
-    //     if(e.aboutButton, e.aboutClose) {
-    //         portfooModal.display.style = "none";
-    //     }elseif(e.portfooButton, e.portClose) {
-    //         aboutModal.display.style = "none";
-    //     }
-    
-    // }
 });
+
+
+
+
+
