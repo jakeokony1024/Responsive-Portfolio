9bc9f48b90c2035adde2aaf87eeb98360cdb4107
Jakeokony1024 <jakeokony@gmail.com>
2021-05-18 20:40:34 -0400

    updated project modal, now displaying seperate info

--- assets copy/javascript/app.js
+++ assets copy/javascript/app.js
@@ -1,10 +1,10 @@
 //////////Portfoo Objects/////////////
 $(document).ready(function(){
-
+    
     const projectArray = [
         {
             id: "proj0",
-            image: "assets copy/images/workoutplaceholder1.jpeg",
+            image: "assets copy/images/jquerylogo.PNG",
             title: "Corey's Active Fitness",
             description: "A Personal Trainer's Website that allows users to store their personal data, schedule classes leave comments / feedback for the trainer.",
             links: {
@@ -25,7 +25,7 @@ $(document).ready(function(){
         },
             {
                 id: "proj1",
-                image: "assets copy/images/javascriptlogo.PNG",
+                image: "assets copy/images/htmllogo.PNG",
                 title: "Mikes Movie Search",
                 description: "This site is designed to help the client search for a movie or tv show and directs them to the streaming service that is offering that show or movie.",
                 links : {
@@ -42,7 +42,7 @@ $(document).ready(function(){
             },  
             {
                 id: "proj2",
-                image: "./assets copy/images/workoutplaceholder1.jpeg",
+                image: "./assets copy/images/csslogo.PNG",
                 title: "Image Click Game",
                 description: "This site gives the user a table of images and directions to try and click on each different image no more than once. Once an image is clicked, the entire table is rearranged in a random order, and if the same image is clicked twice, the score is set back to zero. ",
                 links : {
@@ -59,7 +59,7 @@ $(document).ready(function(){
             },  
             {
                 id: "proj3",
-                image: "./assets copy/images/workoutplaceholder1.jpeg",
+                image: "./assets copy/images/reactlogo.PNG",
                 title: "Friend Finder App",
                 description: "This site has the user go through a list of questions, and based on the answers gives them a 'friend' they are most compatible with.",
                 links : {
@@ -76,7 +76,7 @@ $(document).ready(function(){
             },
             {
                 id: "proj4",
-                image: "./assets copy/images/workoutplaceholder1.jpeg",
+                image: "./assets copy/images/nodejslogo.PNG",
                 title: "Giphy Image Search",
                 description: "This site allows users to search for a list of GIF images populated from the Giphy API. Images can be clicked to show the animation, and clicked again to stop animation.",
                 links : {
@@ -91,6 +91,7 @@ $(document).ready(function(){
                 },
         },
 ];
+    let softWareImgArray = [];
     let projectTitle;
     let projectDescription;
     let projectImage ;
@@ -106,14 +107,14 @@ $(document).ready(function(){
             portfolioModal.style.display = "block";
             aboutModal.style.display = "none";
             if ($('#projectButtons').is(':empty')){
-                projectButtons();
+                projectCards();
             }else{
                 return;
             }
         }
         else if (this.id == 'aboutClose') {
             aboutModal.style.display = "none";
-            projectModal.style.display = "block";
+            
         }
         else if (this.id == 'portClose') {
             portfolioModal.style.display = "none";
@@ -126,37 +127,76 @@ $(document).ready(function(){
     $('body').on('click', 'button.proj-buttons', function(){
         if (this.id == 'proj0'){
             projectModal.style.display = "block";
+            projectTitle = projectArray[0].title;
+            projectImage = projectArray[0].image;
+            projectLinkGH = projectArray[0].links.github;
+            projectLinkWS = projectArray[0].links.website;
+            $("#project-title").text(projectTitle);
+            $("#project-image").attr("src", projectImage);  
+            
 
         } else if (this.id == 'proj1') {
+            projectModal.style.display = "block";
+            projectTitle = projectArray[1].title;
+            projectImage = projectArray[1].image;
+            projectLinkGH = projectArray[1].links.github;
+            projectLinkWS = projectArray[1].links.website;
+            $("#project-title").text(projectTitle);
+            $("#project-image").attr("src", projectImage);  
             
-        projectModal.style.display = "block";
+            
         } else if (this.id == 'proj2') {
-            
-        projectModal.style.display = "block";
+            projectModal.style.display = "block";
+            projectTitle = projectArray[2].title;
+            projectImage = projectArray[2].image;
+            projectLinkGH = projectArray[2].links.github;
+            projectLinkWS = projectArray[2].links.website;
+            $("#project-title").text(projectTitle);
+            $("#project-image").attr("src", projectImage);
+        
         } else if (this.id == 'proj3') {
-            
-        projectModal.style.display = "block";
+            projectModal.style.display = "block";
+            projectTitle = projectArray[3].title;
+            projectImage = projectArray[3].image;
+            projectLinkGH = projectArray[3].links.github;
+            projectLinkWS = projectArray[3].links.website;
+            $("#project-title").text(projectTitle);
+            $("#project-image").attr("src", projectImage);
+        
         } else if (this.id == 'proj4') {
-            
-        projectModal.style.display = "block";
-        }
+            projectModal.style.display = "block";
+            projectTitle = projectArray[4].title;
+            projectImage = projectArray[4].image;
+            projectLinkGH = projectArray[4].links.github;
+            projectLinkWS = projectArray[4].links.website;
+            $("#project-title").text(projectTitle);
+            $("#project-image").attr("src", projectImage);
+        
+        } 
     });
     
-
-    let projectButtons = () => {
-        //iterate through projectArray object, grab out titles, descriptions, images, links, software list//
-        projectArray.forEach((element) => {
+    let projectCards = () => {
+        // iterate through projectArray object, grab out titles, descriptions, images//
+        projectArray.forEach((element, index) => {
+            
             a = $("<button class='btn btn-primary proj-buttons' id='"+ element.id +"'>");
             b = $("<p class='card-text'>");
             c = $("<img alt='image' class='card-img-top'>").attr("src", element.image);
             a.text(element.title);
             b.text(element.description);
-            // $("#image-target").append(c);
-            $("#projectButtons").append(a,b,c);
+            $("#image-target").append(c);
+            $("#projectButtons").append(c,a,b);
 
             
         })
     }
+
+    let project0 = () => {
+        
+
+        $()
+    }
+
 });
 
 
